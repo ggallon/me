ebbe00ea0836445aa26cd60c3a811eb83d6e7b82
Gwenaël Gallon <gwenael.gallon@mac.com>
2020-10-25 17:05:40 +0100

    Add reportWebVitals

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import 'styles/globals.css'
 
+export function reportWebVitals(metric) {
+	const body = JSON.stringify(metric)
+	console.log('body', metric)
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
