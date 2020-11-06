a78ac7f3b01c465c91bda26c0e63ce0cca9a6834
Gwenaël Gallon <ggallon@users.noreply.github.com>
2020-11-06 16:03:38 +0100

    Custom http headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' vitals.vercel-analytics.com",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'sameorigin',
+          },
+          {
+            key: 'X-Xss-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ]
+  }
 }
