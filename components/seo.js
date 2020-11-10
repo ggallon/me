a674be9dae1ddb9327aedc5217d40d98db773de5
Gwenaël Gallon <ggallon@users.noreply.github.com>
2020-11-10 16:49:39 +0100

    Add open graph image

--- components/seo.js
+++ components/seo.js
@@ -12,6 +12,9 @@ export default function SEO() {
       <meta property="og:type" content="website" />
       <meta property="og:description" content={description} />
       <meta property="og:url" content="https://gwenaelgallon.fr" />
+      <meta property="og:image" content="https://gwenaelgallon.fr/og.png" />
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="720" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
