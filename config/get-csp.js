6faa674687bdd324e41ccc843b1bc84ce1c88e21
Gwenaël Gallon <ggallon@users.noreply.github.com>
2020-11-23 10:51:45 +0100

    Update getCsp() function

--- config/get-csp.js
+++ config/get-csp.js
@@ -2,33 +2,37 @@ const prod = process.env.NODE_ENV == 'production'
 const vercelVitalsUrl = 'vitals.vercel-analytics.com'
 
 function getCsp() {
-  let csp = ''
-  /* fetch directives */
-  csp += `connect-src 'self' ${vercelVitalsUrl}; `
-  csp += `default-src 'self'; `
-  csp += `font-src 'self'; `
-  csp += `frame-src 'self'; `
-  csp += `img-src 'self'; `
-  csp += `manifest-src 'self'; `
-  csp += `media-src 'self'; `
-  csp += `object-src 'none'; `
-  csp += `prefetch-src 'self'; `
-  // NextJS requires 'unsafe-eval' in dev
-  csp += `script-src 'self' ${
-    prod ? '' : "'unsafe-eval'"
-  } 'unsafe-inline' ${vercelVitalsUrl}; `
-  // NextJS requires 'unsafe-inline' in dev
-  csp += `style-src ${prod ? "'self'" : "'unsafe-inline'"}; `
-  csp += `worker-src 'self'; `
-  /* Document directives */
-  csp += `base-uri 'self'; `
-  /* Navigation directives */
-  csp += `form-action 'self'; `
-  csp += `frame-ancestors 'self'; `
-  /* Other directives */
-  csp += prod ? `block-all-mixed-content; ` : ''
-  csp += prod ? `upgrade-insecure-requests;` : ''
-  return csp
+  const csp = [
+    /* fetch directives */
+    `connect-src 'self' ${
+      prod ? vercelVitalsUrl : ''
+    }`,
+    `default-src 'self'`,
+    `font-src 'self'`,
+    `frame-src 'self'`,
+    `img-src 'self'`,
+    `manifest-src 'self'`,
+    `media-src 'self'`,
+    `object-src 'none'`,
+    `prefetch-src 'self'`,
+    // NextJS requires 'unsafe-eval' in dev
+    `script-src 'self' ${
+      prod ? '' : "'unsafe-eval'"
+    } 'unsafe-inline' ${vercelVitalsUrl}`,
+    // NextJS requires 'unsafe-inline' in dev
+    `style-src ${prod ? "'self'" : "'unsafe-inline'"}`,
+    `worker-src 'self'`,
+    /* Document directives */
+    `base-uri 'self'`,
+    /* Navigation directives */
+   `form-action 'self'`,
+   `frame-ancestors 'self'`,
+   /* Other directives */
+   prod ? `block-all-mixed-content` : '',
+   prod ? `upgrade-insecure-requests;` : ''
+  ]
+
+  return csp.join('; ')
 }
 
 exports.getCsp = getCsp
