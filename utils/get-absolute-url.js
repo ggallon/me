82330e774476877b6d8f635fd6687052f174e63d
Gwenaël Gallon <ggallon@users.noreply.github.com>
2020-11-27 15:05:45 +0100

    Optimize getAbsoluteURL()

--- utils/get-absolute-url.js
+++ utils/get-absolute-url.js
@@ -1,7 +1,12 @@
-const isProd = process.env.NODE_ENV === 'production'
+import { isProd, isDev } from '@utils/get-env'
 
 export const getAbsoluteURL = (path, lang) => {
-  const baseURL = isProd ? `https://${process.env.APP_URL}` : "http://localhost:3000"
+  console.log('isProd', isProd)
+  const baseURL = isProd
+    ? 'https://gwenaelgallon.fr'
+    : isDev
+      ? 'http://localhost:3000'
+      : `https://${process.env.APP_URL}`
   const cleanPath = path ? path : ''
   const cleanLang = lang ? `/${lang}` : ''
   return `${baseURL}${cleanLang}${cleanPath}`
