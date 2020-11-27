b535662512fb81f01a18c6cdd365150974053945
Gwenaël Gallon <ggallon@users.noreply.github.com>
2020-11-27 11:40:29 +0100

    Move global head elements to _document

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,27 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="fr">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body className="bg-white dark:bg-black text-black dark:text-white">
           <Main />
           <NextScript />
