684d3d43a92e3277a118be7374c78d33bbf81fcb
Gwenaël Gallon <gwenael.gallon@mac.com>
2021-10-14 22:52:15 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,24 +17,28 @@ module.exports = bundleAnalyzer({
         locale: false,
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: getCsp(),
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
           },
           {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
           },
           {
             key: 'X-Frame-Options',
-            value: 'sameorigin',
-          },
-          {
-            key: 'X-Xss-Protection',
-            value: '1; mode=block',
+            value: 'SAMEORIGIN'
           },
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: getCsp(),
           },
         ],
       },
