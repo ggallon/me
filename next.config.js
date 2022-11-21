1f3fc01029b5a481edb426b7b4c9199730347efa
Gwenaël Gallon <gwenael.gallon@mac.com>
2022-11-21 15:03:02 +0100

    Update security headers rules

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-const { getCsp } = require('./config/get-csp')
 const bundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: !!process.env.BUNDLE_ANALYZE,
 })
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = bundleAnalyzer({
-  swcMinify: true,
   reactStrictMode: true,
 
   experimental: {
@@ -17,54 +18,62 @@ module.exports = bundleAnalyzer({
     return [
       {
         source: '/(.*)',
-        locale: false,
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: getCsp(),
-          },
-        ],
-      },
-      {
-        source: '/_next/static/(.*)',
-        locale: false,
-        headers: [
-          {
-            key: 'Vary',
-            value: 'Accept-Encoding',
-          },
-        ],
-      },
-      {
-        source: '/(.jpg|.png|.webp|.gif)',
-        locale: false,
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'public,max-age=31536000,immutable',
-          },
-        ],
-      },
-    ]
-  },
+        headers: securityHeaders
+      }
+    ];
+  }
 })
+
+// https://nextjs.org/docs/advanced-features/security-headers
+const ContentSecurityPolicy = `
+    connect-src *;
+    default-src 'self';
+    font-src 'self';
+    img-src * blob: data:;
+    media-src 'none';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self';
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, '')
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()'
+  }
+];
