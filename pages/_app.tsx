4829f17e870c117ce762a74b024f6f76431bf60f
Gwenaël Gallon <ggallon@users.noreply.github.com>
2023-01-03 19:31:09 +0100

    Last touch (#33)
    * feat: remove jest
    
    * feat: move to typescript
    
    * feat: update Content Security Policy
    
    * fix: update CSP for vercel.live
    
    * fix: update CSP for vercel.live (Frame)
    
    * chore: bump dependencies
    
    * fix: spaces in CSP
    
    * feat: reorder import
    
    * feat: Add ToggleTheme
    
    * chore: add plugins to prettier
    
    * feat: change theme order
    
    * fix: responsive
    
    * chore: run prettier
    
    * feat: use a custom Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
-import { Inter } from '@next/font/google'
+import localFont from '@next/font/local'
 import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from 'next-themes'
-
 import type { AppProps } from 'next/app'
 import '#/assets/main.css'
 
-const interVariable = Inter({
+const interVariable = localFont({
+  src: '../assets/fonts/Inter-roman-latin.var.woff2',
   variable: '--font-inter',
-  subsets: ['latin'],
 })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
